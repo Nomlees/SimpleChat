4ebd7dd1378dd1319a703cd0de426416f23b71c6
Nomlees <nomlees@bk.ru>
2018-03-25 16:12:02 +0300

    Initial commit

---
+++ Network/src/ru/zak/network/TCPConnectionListener.java
@@ -0,0 +1,16 @@
+package ru.zak.network;
+
+import java.io.IOException;
+
+public interface TCPConnectionListener {
+
+    void onConnectionReady(TCPConnection tcpConnection);
+
+    void onReceiveString(TCPConnection tcpConnection, String s);
+
+    void onDisconnect(TCPConnection tcpConnection);
+
+    void onException(TCPConnection tcpConnection, IOException e);
+
+}
+
